47323a2ea8cc7e3286f853eb058956a5a588cdeb
Julius <medicjue@gmail.com>
2018-01-21 00:04:04 +0800

    Character Embedding based on Text Understanding from Scratch thesis

--- textFeatUtils/textFeatUtils.py
+++ textFeatUtils/textFeatUtils.py
@@ -193,3 +193,48 @@ class TfIdfEncoder:
     def fit_transform(self, documents):
         self.fit(documents)
         return self.transform(documents)
+    
+class CharEmbeddedEncoder:
+    """
+    An encoder for character embedding based on "Text Understanding from Scratch"
+        URL: https://arxiv.org/pdf/1502.01710.pdf
+    """
+    np = __import__('numpy')
+    mp = __import__('multiprocessing')
+    def __init__(self, n_jobs=2, sequence_max_length=1014):
+        self.alphabet =  'abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:\'"/|_#$%^&*~`+=<>()[]{} \n'
+        self.char_dict = {}
+        self.sequence_max_length = sequence_max_length
+        self.n_jobs = n_jobs
+        for i,c in enumerate(self.alphabet):
+            self.char_dict[c] = i
+        self.char_dict_len = len(self.char_dict)
+                          
+    def char2vec(self, text):
+        data = self.np.ones(self.sequence_max_length) * self.char_dict_len
+        for i in range(len(text)):
+            if text[i] in self.char_dict:
+                data[i] = self.char_dict[text[i]]
+            else:
+                data[i] = self.char_dict_len - 1
+            if i > self.sequence_max_length:
+                return data
+        return data
+    
+    def transform_job(self, documents):
+        char_vecs = []
+        for document in documents:
+            char_vecs.append(self.char2vec(document))
+        return self.np.asarray(char_vecs, dtype=int)
+    
+    def transform(self, documents):
+        if self.n_jobs > 1:
+            pool = self.mp.Pool(self.n_jobs)
+            split_documents = []
+            split_len = int(len(documents)/self.n_jobs)
+            for i in range(self.n_jobs):
+                split_documents.append(documents[(i*split_len):((i+1)*split_len)])
+            res = pool.map(self.transform_job, split_documents)
+            return self.np.concatenate(res, axis=0)
+        else:
+            return self.transform_job(documents)
